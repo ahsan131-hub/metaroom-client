a4d043f694f69d704bc2381beb14870d192aa50f
ahsan131-hub <mahsan.bscsf19@iba-suk.edu.pk>
2023-05-17 09:55:49 +0500

    changed url backedn

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri:
     process.env.GRAPHQL_API ||
-    'http://ec2-3-88-175-192.compute-1.amazonaws.com',
+    'https://ec2-3-95-186-84.compute-1.amazonaws.com/',
   // link: authLink.concat(httpLink),
   // headers: {
   //   authorization: authToken(),
