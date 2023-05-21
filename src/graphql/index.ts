c0fed68a2d8bb1dd46f0ab3bace25db8abd9fb33
ahsan131-hub <mahsan.bscsf19@iba-suk.edu.pk>
2023-05-22 02:41:15 +0500

    chane url server

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.GRAPHQL_API || 'http://localhost:4000',
+  uri: process.env.GRAPHQL_API || 'https://metaroom.store/',
   // link: authLink.concat(httpLink),
   // headers: {
   //   authorization: authToken(),
